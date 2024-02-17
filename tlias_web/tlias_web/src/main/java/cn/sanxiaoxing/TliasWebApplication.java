77196ad3d99239f9dbb7f3f7ec84b64f6dab126f
SanXiaoXing <1137199378@qq.com>
2024-02-17 20:12:14 +0800

    新增项目预览

---
+++ tlias_web/tlias_web/src/main/java/cn/sanxiaoxing/TliasWebApplication.java
@@ -0,0 +1,13 @@
+package cn.sanxiaoxing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TliasWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TliasWebApplication.class, args);
+    }
+
+}
